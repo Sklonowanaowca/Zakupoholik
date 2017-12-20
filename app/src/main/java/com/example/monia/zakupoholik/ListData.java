0e87688b95e8de1ad6affbedf723b841f4e2af9a
Monika <sklonowanaowca@gmail.com>
2017-12-20 20:42:42 +0100

    create SQLiteDatabase, but not use yet

---
+++ app/src/main/java/com/example/monia/zakupoholik/ListData.java
@@ -0,0 +1,12 @@
+package com.example.monia.zakupoholik;
+
+/**
+ * Created by Monia on 2017-12-20.
+ */
+
+public class ListData {
+    public int idLista;
+    public String nazwaListy;
+    public String dataZakupow;
+    public double kosztZakupow;
+}
